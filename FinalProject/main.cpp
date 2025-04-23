ca41fc60040f31db0a1f3858304a8fb1ed594b47
Karkaranos <karkaranosyt@gmail.com>
2025-04-22 20:59:11 -0500

    searching and cache logic

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>>
+#include <unordered_map>
 using namespace std;
 class CSVReader {
 private:
@@ -49,10 +50,83 @@ private:
     }
 };
 
+class Cache {
+public:
+    unordered_map<string, pair<int,vector<string>>> recent;
 
+    bool findEntry(string countryCode, string cityCode) {
+        string key = countryCode + cityCode;
+        if (recent.find(key) != recent.end()) {
+            return true;
+        }
+        return false;
+    }
+    void enqueue(vector<string> newEntry) {
+        //If the entry is already stashed in the cache
+        if (findEntry(newEntry[0], newEntry[1])) {
+            //Entry already exists; adjust its position
+            auto entry = recent[(newEntry[0]+newEntry[1])];
+            int lastIndex = entry.first;
+            if (lastIndex==1) {
+                return;
+            }
+            else
+            {
+                for (auto entry : recent) {
+                    if (entry.second.first < lastIndex) {
+                        entry.second.first++;
+                    }
+                }
+                entry.first = 1;
+                recent[(newEntry[0]+newEntry[1])] = entry;
+                cout << "Entry updated";
+                return;
+            }
+
+        }
+        //If the entry is new
+
+        //Make space for it
+        for (auto entry : recent) {
+            entry.second.first++;
+            if (entry.second.first > 9) {
+                recent.erase(entry.first);
+            }
+        }
+
+        recent.insert(make_pair((newEntry[0]+newEntry[1]), make_pair(1, newEntry)));
+        cout << "New entry added";
+
+    }
+
+};
+
+void DisplayAndUpdate(vector<string> city, Cache& cache)
+{
+    cout << city[1] << " Population: " << city[2] << endl;
+    cache.enqueue(city);
+
+}
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    CSVReader reader;
+    string file = 
+    Cache cache;
+    string end = "~";
+    string in = "";
+    string code, city;
+    while (in != end) {
+        cout << "To terminate program, type ~" << endl;
+        cout << "Enter country code: ";
+        cin >> in;
+        code = in;
+        cout << "Enter city name: ";
+        cin >> in;
+        city = in;
+
+        vector<string> result = reader.readEntry(file, code, city);
+
+    }
     return 0;
 }
