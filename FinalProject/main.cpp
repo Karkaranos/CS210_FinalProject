684fe394696bc70a084371bf6b7a7b87dce50b59
Karkaranos <karkaranosyt@gmail.com>
2025-04-29 16:56:10 -0500

    LFU Cache

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -52,9 +52,23 @@ private:
 
 class Cache {
 public:
+
+    virtual bool findEntry(string countryCode, string cityCode);
+    virtual void remove();
+    virtual void addToCache(vector<string> newEntry);
+    virtual void view();
+    virtual void displayAndUpdate(vector<string> city);
+
+};
+
+class LFUCache {
+public:
+    LFUCache() {
+        recent.clear();
+    }
     unordered_map<string, pair<int,vector<string>>> recent;
 
-    bool findEntry(string countryCode, string cityCode) {
+    bool findEntry(string countryCode, string cityCode)  {
         string key = countryCode + cityCode;
         if (recent.find(key) != recent.end()) {
             return true;
@@ -62,49 +76,32 @@ public:
         return false;
     }
 
-    void removeOldest(bool found, unordered_map<string,int> visited) {
-        vector<pair<string, int>> newPos;
+    void remove()  {
+        pair<int, string> leastFrequent;
+        leastFrequent.first = INT_MAX;
         for (auto entry : recent) {
-            if (visited.find(entry.first)!=visited.end()) {
-                continue;
+            if (entry.second.first < leastFrequent.first) {
+                leastFrequent.first = entry.second.first;
+                leastFrequent.second = entry.first;
             }
-            if (!found && entry.second.first > 9) {
-                recent.erase(entry.first);
-                return;
-            }
-            visited[entry.first] = entry.second.first+1;
         }
-    }
-    void enqueue(vector<string> newEntry) {
-        //Make space for it and adjust positions
-        unordered_map<string,int> newPos;
-        bool found = findEntry(newEntry[0], newEntry[1]);
-        removeOldest(found, newPos);
-        for (auto entry : recent)
-        {
-            recent[entry.first] = make_pair(entry.second.first+1, entry.second.second);
 
+        recent.erase(leastFrequent.second);
+
+    }
+    void addToCache(vector<string> newEntry)  {
+        //Make space for it and adjust positions
+        if (recent.size() > 2) {
+            remove();
         }
+        bool found = findEntry(newEntry[0], newEntry[1]);
         //If the entry is already stashed in the cache
         if (found) {
-            //Entry already exists; adjust its position
+            //Entry already exists; adjust its frequency
             auto entry = recent[(newEntry[0]+newEntry[1])];
-            int lastIndex = entry.first;
-            if (lastIndex==1) {
-                return;
-            }
-            else
-            {
-                for (auto entry : recent) {
-                    if (entry.second.first < lastIndex) {
-                        entry.second.first++;
-                    }
-                }
-                entry.first = 1;
-                recent[(newEntry[0]+newEntry[1])] = entry;
-                return;
-            }
-
+            entry.first++;
+            recent[(newEntry[0]+newEntry[1])] = entry;
+            return;
         }
 
         //Add the new city
@@ -112,7 +109,7 @@ public:
 
     }
 
-    void view() {
+    void view()  {
         for (auto entry : recent) {
             cout << entry.second.first << " ";
             for (int i=0; i<3;i++) {
@@ -122,20 +119,18 @@ public:
         }
     }
 
+    void displayAndUpdate(vector<string> city)  {
+        cout << city[1] << " Population: " << city[2] << endl;
+        addToCache(city);
+    }
+
 };
-
-void DisplayAndUpdate(vector<string> city, Cache& cache)
-{
-    cout << city[1] << " Population: " << city[2] << endl;
-    cache.enqueue(city);
-
-}
 
 
 int main() {
     CSVReader reader;
     string file = "world_cities.csv";
-    Cache cache;
+    LFUCache* c = new LFUCache();
     string end = "~";
     string in = " ";
     string code, city;
@@ -156,13 +151,13 @@ int main() {
 
         vector<string> result = reader.readEntry(file, code, city);
         if (result.size()>0) {
-            DisplayAndUpdate(result, cache);
+            c->displayAndUpdate(result);
         }
         else {
             cout << city << " not found." << endl;
         }
 
-        cache.view();
+        c->view();
     }
     return 0;
 }
